5949184ab6c9885eb16a34f855317240da98c513
Mikhail Efremov <jakutenshi@gmail.com>
2022-11-29 22:28:57 +0600

    fix remotes

--- fetch.sh
+++ fetch.sh
@@ -6,7 +6,7 @@ git fetch Kirollbas        # Депрейс Александр
 #git fetch Skibiduika       # Заремба Никита       
 #git fetch Keyrek           # Качабеков Эльхан     
 git fetch Cottoffey        # Кошкин Евгений       
-#git fetch Pavel7004        # Ковалёв Павел        
+git fetch Pavel7004        # Ковалёв Павел        
 #git fetch kulikov02        # Куликов Максим       
 #git fetch Remaf1n          # Крутских Глеб        
 #git fetch gosheno          # Малимон Георгий      
