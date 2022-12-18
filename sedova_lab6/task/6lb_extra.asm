11519512fb9e86200c32c0814b8613c3c9eabe08
EllaSedova <elly41554@yandex.ru>
2022-12-18 20:47:54 +0300

    Sedova lab6

---
+++ sedova_lab6/task/6lb_extra.asm
@@ -0,0 +1,70 @@
+.486
+.MODEL FLAT
+.CODE
+public func
+func proc PROC C array:dword, 
+Numbers:dword, LGrInt:dword, NInt:dword, result:dword, maxim: dword
+
+push eax
+push ebx
+push edi
+push esi
+
+mov esi, array
+mov edi, LGrInt
+mov eax, 0
+
+start: 
+	mov ebx, 0	;индекс текущего интервала
+	next_border:
+ 		cmp ebx, NInt
+		jge this_border		;если ebx>=NInt выходим из цикла
+		push eax
+		mov eax, [esi + 4 * eax]	;кладем в eax элемент массива array
+		cmp eax, [edi + 4 * ebx]	;сравниваем элемент с текущей левой границей
+		pop eax
+		jl this_border	;если eax<[edi + 4 * ebx] выходим из цикла
+		inc ebx 
+		jmp next_border	;иначе переходим к следующему интервалу
+
+	this_border:	;когда вышли из цикла
+		dec ebx
+		cmp ebx, -1
+		je next_num		;если вышли за границу интервалов 
+		mov edi, result
+		push eax
+		mov eax, [edi + 4 * ebx]	;помещаем в eax помещаем элемент массива 
+									;result с индексом ebx
+		inc eax
+		mov [edi + 4 * ebx], eax	;устанавливаем полученное значение 
+									;обратно в массив result
+		pop eax
+		push eax
+		push edi
+		mov edi, maxim
+		mov eax, [esi + 4*eax]
+		cmp eax, [edi + 4*ebx]; <=
+		jle less
+		mov [edi + 4*ebx], eax
+		less:
+		pop edi
+		pop eax
+
+		mov edi,LGrInt
+
+	next_num:
+		inc eax		;индекс следующего числа
+		cmp eax, Numbers
+		jg exit
+jmp start		;начинаем новую итерацию
+
+exit:
+pop esi
+pop edi
+pop ebx
+pop eax
+
+ret
+
+func ENDP
+END
---
+++ sedova_lab6/task/lab6_extra.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <fstream>
+#include <random>
+#include <windows.h>
+
+extern "C" void(func(int* array, int Numbers,
+	int* LGrInt, int NInt, int* result, int* maxim)); 
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+	std::ofstream Result;
+    Result.open("result.txt", std::ios_base::out);
+
+    int Numbers;
+    std::cout << "Длина массива псевдослучайных целых чисел: \n";
+    std::cin >> Numbers;
+	if (Numbers <= 0 || Numbers > 16 * 1024) {
+		printf("Неверная длина массива\n");
+		return 1;
+	}
+    int* array = new int[Numbers];
+
+    int left;
+    std::cout << "Левая граница массива: \n";
+    std::cin >> left;
+
+    int right;
+    std::cout << "Правая граница массива: \n";
+    std::cin >> right;
+	if (left > right) {
+		printf("Неверный диапазон генерации чисел\n");
+		return 1;
+	}
+
+	for (int i = 0; i < Numbers; ++i) {
+		array[i] = rand() % (right - left + 1) + left;
+	}
+
+    int NInt;
+    std::cout << "Количество интервалов разбиения: ";
+    std::cin >> NInt;
+	if (NInt <= 0) {
+		printf("Неверное количество интервалов разбиения\n");
+		return 1;
+	}
+
+	int* LGrInt = new int[NInt];
+	int tmp;
+	for (int i = 0; i < NInt; i++) {
+		std::cout << "Введите левую границу интервала[" << i+1 << "]: ";
+		std::cin >> tmp;
+		LGrInt[i] = tmp;
+		if (LGrInt[i] < left || LGrInt[i] > right || 
+			i > 0 && LGrInt[i] < LGrInt[i - 1] || LGrInt[i] == LGrInt[i - 1]) {
+			printf("Некорректное значение левой границы\n");
+			free(LGrInt);
+			return 1;
+		}
+	}
+    
+	int* maxim = new int[NInt];
+	for (int i = 0; i < NInt; i++) {
+		maxim[i] = left;
+	}
+
+    int* result = new int[NInt] {0};
+    func(array, Numbers, LGrInt, NInt, result, maxim);
+
+	std::cout << "Массив: ";
+	Result << "Массив: ";
+	for (int i = 0; i < Numbers; ++i) {
+		std::cout << array[i] << ' ';
+		Result << array[i] << ' ';
+	}
+	std::cout << '\n';
+	Result << '\n';
+	std::cout << "Номер интервала:\t" << "Левая граница:\t" << "Количество чисел, попавших в интервал:\t" << "Максимум:" << '\n';
+	Result << "Номер интервала:\t" << "Левая граница:\t" << "Количество чисел, попавших в интервал:\t"<<"Максимум:"<<'\n';
+	for (int i = 0; i < NInt; ++i) {
+		std::cout << "    " << i + 1 << "\t        \t" << LGrInt[i] << "\t\t" << result[i] << "\t\t\t\t\t"<<maxim[i]<<'\n';
+		Result << "    " << i + 1 << "\t        \t" << LGrInt[i] << "\t\t" << result[i] << '\n';
+	}
+
+    delete[] array;
+    delete[] LGrInt;
+    delete[] result;
+    Result.close();
+    return 0;
+}
