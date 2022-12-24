9d5bf32b89e5638e3010ee576695a66494fd5da8
Mikhail Efremov <jakutenshi@gmail.com>
2022-12-24 14:37:12 +0600

    Bogdanov's lab6 is accepted

---
+++ bogdanov/lab6/l6.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+#include <math.h>
+
+using namespace std;
+
+extern "C" void counter(int* array, int array_size, int* LGrInt, int NInt, int* result_array);
+
+int main()
+{
+	srand(time(0));
+	int NumRanDat, Xmin, Xmax, NInt;
+	cin >> NumRanDat >> Xmin >> Xmax >> NInt;
+	int* arr = new int[NumRanDat];
+	for (int i = 0; i < NumRanDat; ++i)
+	{
+		arr[i] = Xmin + rand() % abs(Xmax - Xmin+1);
+	}
+	
+	int* LGrInt = new int[NInt];
+	LGrInt[0] = Xmin;
+	for (int i = 0; i < NInt; ++i)
+	{
+		cin >> LGrInt[i];
+	}
+	int* ans = new int[NInt] {0};
+	counter(arr, NumRanDat, LGrInt, NInt, ans);
+	//freopen("out.txt", "w", stdout);
+	for (int i = 0; i < NInt; ++i)
+	{
+		cout << i + 1 << "	" << LGrInt[i] << "	" << ans[i] << endl;;
+	}
+	for (int i = 0; i < NumRanDat; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
---
+++ bogdanov/lab6/task.asm
@@ -0,0 +1,63 @@
+.686p
+.MODEL FLAT, C
+.CODE
+counter PROC C USES EDI ESI, arr:dword, siz:dword, LGrInt:dword, NInt:dword, ans:dword
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push edi
+	push esi
+
+	mov ecx, siz
+	mov esi, arr
+	mov edi, LGrInt
+	mov eax, 0
+
+	CYCLE:
+		mov ebx, 0
+	SEARCH:
+ 		cmp ebx, NInt
+		jge O
+
+		push eax
+		mov eax, [esi + 4 * eax]
+		mov edx,eax
+		cmp eax, [edi + 4 * ebx]
+		pop eax
+		jl O
+
+		add ebx,1
+		jmp SEARCH
+
+	O:
+		sub ebx,1
+		cmp ebx, -1
+		je NUMBER
+
+		mov edi, ans
+
+		push eax
+		mov eax, [edi + 4 * ebx]
+		add eax,edx
+		mov [edi + 4 * ebx], eax
+		pop eax
+
+		mov edi, LGrInt
+
+	NUMBER:
+		add eax,1
+
+	loop CYCLE
+
+	pop esi
+	pop edi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+
+ret
+
+counter ENDP
+END
